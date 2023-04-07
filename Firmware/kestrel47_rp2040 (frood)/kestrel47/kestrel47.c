fffefa1f653b9615771cb135190521ef4fe53cf4
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2023-04-07 17:17:03 +0200

    different firmware

---
+++ Firmware/kestrel47_rp2040 (frood)/kestrel47/kestrel47.c
@@ -0,0 +1,1 @@
+#include "kestrel47.h"
---
+++ Firmware/kestrel47_rp2040 (frood)/kestrel47/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0x63, 0xDA, 0xE4, 0xF9, 0xF0, 0x7F, 0xA0}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
